0cbac08dc723a7922abcbbe7fe85424e34bfb937
Peter Reutemann <fracpete@gmail.com>
2017-05-15 11:54:38 +1200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # processoutput4j
 Simple Java library for capturing stdout/stderr of processes launched from within Java.
+
+## Examples
+The following executes the process and outputs any data from stdout/stderr
+once the process has finished:
+```java
+import com.github.fracpete.processoutput4j.output.ConsoleOutputProcessOutput;
+...
+String[] cmd = new String[]{"/bin/ls", "-la", "/some/where"};
+ProcessBuilder builder = new ProcessBuilder();
+builder.command(cmd);
+new ConsoleOutputProcessOutput(builder);
+```
+
+The following executes the process and outputs any data from stdout/stderr
+once the process has finished:
+```java
+import com.github.fracpete.processoutput4j.output.CollectingProcessOutput;
+...
+String[] cmd = new String[]{"/bin/ls", "-la", "/some/where"};
+ProcessBuilder builder = new ProcessBuilder();
+builder.command(cmd);
+CollectingProcessOutput output = new CollectingProcessOutput(builder);
+System.out.println("exit code: " + output.getExitCode());
+System.out.println(output.getStdOut());
+```
---
+++ src/main/java/com/github/fracpete/processoutput4j/output/CollectingProcessOutput.java
@@ -0,0 +1,127 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * CollectingProcessOutput.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package com.github.fracpete.processoutput4j.output;
+
+import com.github.fracpete.processoutput4j.reader.CollectingProcessReader;
+
+/**
+ * Collects the process output (stdout and stderr) and makes them available
+ * once the process finishes.
+ *
+ * @author fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 6502 $
+ */
+public final class CollectingProcessOutput
+  extends AbstractProcessOutput {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 1902809285333524039L;
+
+  /** the stdout content. */
+  protected StringBuilder m_StdOut;
+
+  /** the stderr content. */
+  protected StringBuilder m_StdErr;
+
+  /**
+   * Starts the monitoring process.
+   *
+   * @param cmd		the command that was used
+   * @param env		the environment
+   * @param input	the input to be written to the process, can be null
+   * @param process	the process to obtain the results from
+   * @throws Exception	if collection of data fails
+   */
+  public CollectingProcessOutput(String cmd, String[] env, String input, Process process) throws Exception {
+    this(new String[] {cmd}, env, input, process);
+  }
+
+  /**
+   * Starts the monitoring process.
+   *
+   * @param cmd		the command that was used
+   * @param env		the environment
+   * @param input	the input to be written to the process, can be null
+   * @param process	the process to obtain the results from
+   * @throws Exception	if collection of data fails
+   */
+  public CollectingProcessOutput(String[] cmd, String[] env, String input, Process process) throws Exception {
+    super(cmd, env, input, process);
+  }
+
+  /**
+   * Starts the monitoring process.
+   *
+   * @param builder	the process to obtain the results from
+   * @throws Exception	if collection of data fails
+   */
+  public CollectingProcessOutput(ProcessBuilder builder) throws Exception {
+    super(builder);
+  }
+
+  /**
+   * For initializing the members.
+   */
+  @Override
+  protected void initialize() {
+    super.initialize();
+    m_StdOut = new StringBuilder();
+    m_StdErr = new StringBuilder();
+  }
+
+  /**
+   * Configures the thread for stderr.
+   *
+   * @param process 	the process to monitor
+   * @return		the configured thread, not yet started
+   */
+  protected Thread configureStdErr(Process process) {
+    return new Thread(new CollectingProcessReader(process, false, m_StdErr));
+  }
+
+  /**
+   * Configures the thread for stdout.
+   *
+   * @param process 	the process to monitor
+   * @return		the configured thread, not yet started
+   */
+  protected Thread configureStdOut(Process process) {
+    return new Thread(new CollectingProcessReader(process, true, m_StdOut));
+  }
+
+  /**
+   * Returns the output on stdout.
+   *
+   * @return the output
+   */
+  public String getStdOut() {
+    return m_StdOut.toString();
+  }
+
+  /**
+   * Returns the output on stderr.
+   *
+   * @return the output
+   */
+  public String getStdErr() {
+    return m_StdErr.toString();
+  }
+}
